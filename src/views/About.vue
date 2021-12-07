d8428f2405eed84bc9b2ea3a8e04ce926c77c62d
0101dd <aa09335665@gmail.com>
2021-12-07 15:08:57 +0800

    檔案

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+#about
+  b-container
+    b-row
+      b-col.text-center.m-5(col='12')
+        p
+         | 網頁設計及維護：xxoooo
+         br
+         | 最近更新日期 : {{ date}}
 </template>
+<script>
+export default {
+  data () {
+    return {
+      date: new Date().toLocaleDateString()
+    }
+  }
+}
+</script>
